c7435ec5f169520e2f2cf7184d980e5653f02035
Mark Sirany <marksirany@me.com>
2020-02-05 13:58:27 -0600

    added communication text box/component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar.js';
 import Schedule from './components/Schedule.js';
 import Meals from './components/Meals.js';
+import Annoucements from './components/Announcements';
 
 export default function Nav() {
   return (
     <div>
+
       <Navbar />
+      <Annoucements />
       <h1>Schedule</h1>
       <Schedule />
       <h1>Meals</h1>
