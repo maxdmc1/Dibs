8fbbc91d071ddadf3092852439e5008717c401c9
Mark Sirany <marksirany@me.com>
2020-02-07 20:26:24 -0600

    updated navbar design with colored buttons, and I love it!

--- frontend/src/components/Announcements.js
+++ frontend/src/components/Announcements.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
 import Container from "react-bootstrap/Container";
 
 import ListGroup from 'react-bootstrap/ListGroup'
@@ -11,35 +11,31 @@ export default function Annoucements() {
         <Container>
             <br />
             <Row>
+                
                 <Col>
-                    <>
-                        <ListGroup as="ul">
-                            <ListGroup.Item variant="primary">
-                                Annoucements
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li">Communicate with your group</ListGroup.Item>
-                            <ListGroup.Item as="li" disabled>
-                               
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li"></ListGroup.Item>
-                        </ListGroup>
-                    </>
+                    
+                    <ListGroup as="ul">
+                        <ListGroup.Item variant="primary">
+                            Annoucements
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">Communicate with your group</ListGroup.Item>
+                        <ListGroup.Item as="li" disabled>
+                            
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li"></ListGroup.Item>
+                    </ListGroup>
+                    <ListGroup as="ul">
+                        <ListGroup.Item variant="dark">
+                            Shopping List
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">Add items to your group shopping list</ListGroup.Item>
+                        <ListGroup.Item as="li" disabled>
+
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li"></ListGroup.Item>
+                    </ListGroup>
                 </Col>
-                <Col>
-                    <>
-                        <ListGroup as="ul">
-                            <ListGroup.Item variant="dark">
-                                Shopping List
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li">Add items to your group shopping list</ListGroup.Item>
-                            <ListGroup.Item as="li" disabled>
-                               
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li"></ListGroup.Item>
-                        </ListGroup>
-                    <br />
-                    </>
-                </Col>
+              
 
             </Row>
   
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,42 +1,50 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
 
-function Navbar() {
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+function Menu() {
   return (
-    <div className="container" > 
+    <Container >
+      
+      <Navbar bg="light" variant="light">
+        <Row className="justify-content-md-center">
+          
+            
     
-      <Nav variant="pills" defaultActiveKey="/home">
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Trip Selector
-  </Dropdown.Toggle>
+            <Nav className="mr-auto">
+              <Dropdown>
+                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                  Trip Selector
+              </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Trip 1</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Trip 2</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">trip 3</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Nav.Item>
-          <Nav.Link href="#schedule">Activity Schedule</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#meals">Meal Plan</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#rooms">Bedroooms</Nav.Link>
-        </Nav.Item>
- 
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Trip 1</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Trip 2</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">trip 3</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button variant="primary" href="#schedule">Activity Schedule</Button>
+              <Button variant="success" href="#meals">Meal Plan</Button>
+              <Button variant="danger" href="#rooms">Bedrooms</Button>
+            </Nav>
+         
+        </Row>
+      </Navbar>
+      
 
 
-      </Nav>
-    </div>
+     
+    </Container>
   
      
       
     
   );
 }
-export default Navbar;
+export default Menu;
 
